bca27d35cd20998590d79f92f99674f0d80867e4
clarencelol <clarencekuiek@icloud.com>
2022-01-17 03:29:36 +0800

    wayne: Cleanup
    * Duplicate and some unnecessary stuffs
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- ssos_wayne.mk
+++ ssos_wayne.mk
@@ -11,16 +11,6 @@ $(call inherit-product, device/xiaomi/wayne/device.mk)
 # Inherit ShapeShiftOS product configuration
 $(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 
-# Gapps
-USE_GAPPS := true
-IS_PHONE := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -38,11 +28,16 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := ssos_wayne
 PRODUCT_DEVICE := wayne
 PRODUCT_MODEL := MI 6X
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit ShapeShiftOS Official build stuff.
 SSOS_BUILD_TYPE := OFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_PRODUCT_PROPERTIES += \
   ro.ssos.cpu=SDM660
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_STOCK_ARCORE := true
